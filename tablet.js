92e2b67347fdf4faca66c5d1f9ee9c71fda672ef
sliu384 <sophia.liu384@gmail.com>
2025-08-13 12:51:50 -0700

    reorganized detailed view

--- tablet.js
+++ tablet.js
@@ -27,6 +27,7 @@ async function getAllRecords() {
         newHtml += `
         <div class="tablet-card">
           <div>
+            <a href="tablets.html?id=${data.records[i].id}">
             <img class="card-img-top rounded" alt="${name}" src="${image[0].url}">
             <h3>${name}</h3>
           </div>
@@ -54,7 +55,7 @@ async function getOneRecord(id) {
   };
 
   await fetch(
-    `https://api.airtable.com/v0/appnhz7Yte34rvqwW/tbl1wPbEpjEIvQewE/${id}`, //what link do you put here
+    `https://api.airtable.com/v0/appnhz7Yte34rvqwW/tbl1wPbEpjEIvQewE/${id}`,
     options
   )
     .then((response) => response.json())
@@ -70,23 +71,45 @@ async function getOneRecord(id) {
       let color = data.fields["color gamut (standard)"];
       let tilt = data.fields["tilt support"];
       let link = data.fields["link to item"];
+      let image = data.fields["what it looks like"];
+
+      /* 
+        <div>
+          <h3 class="row>${name}</h3>
+          <div class="row">
+            <div class="col-md-4">
+              <img class="card-img-top rounded logo" alt="${name}" src="${image[0].url}">
+            </div>
+            <div class="col-md-6">
+              <div class="brand container">${brand}</div>
+              <div class="price container">${price}</div>
+              <div class="type container">${type_of_tablet}</div>
+              <div class="size container">${size}</div>
+              <div class="sensitivity container>${sensitivity}</div>
+              <div class = "color container">${color}</div>
+              <div class="tilt container>${tilt}</div>
+              <div class="link container">${link}</div>
+            </div>
+          </div>
+        </div>
+      */
 
       let newHtml = `
-        <div>
-        <div class="row>${name}</div>
-          <div class="col-md-4">
-            //image goes here
-          </div>
-          <div class="col-md-6">
-            <div class="brand container">${brand}</div>
-            <div class="price container">${price}</div>
-            <div class="type container">${type_of_tablet}</div>
-            <div class="size container">${size}</div>
-            <div class="sensitivity container>${sensitivity}</div>
-            <div class = "color container">${color}</div>
-            <div class="tilt container>${tilt}</div>
-            <div class="link container">${link}</div>
-          </div>
+        <div class="row">
+        <div class="col-md-4">
+        <img class="card-img-top rounded logo" alt="${name}" src="${image[0].url}">
+        </div>
+        <div class="col-md-8">
+        <h3>${name}</h3>
+        <h5 class="brand container">${brand}</h5>
+        <p class="price container">$${price}</p>
+        <p class="type container">${type_of_tablet}</p>
+        <p class="size container">Dimensions: ${size}</p>
+        <p class="sensitivity container">${sensitivity}</p>
+        <p class = "color container">Color gamut (standard): ${color}</p>
+        <p class="tilt container">Support Tilt? ${tilt}</p>
+        <a class="link container" target="_blank" href="${link}">Link to Official Website</a>
+        </div>
         </div>
       `;
 
